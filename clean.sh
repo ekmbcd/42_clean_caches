db9cd54e7ca1edfc0814e306de34207cf9eaa8d5
Andrea De felice <ade-feli@e3r9p5.42roma.it>
2021-04-22 16:59:44 +0200

    added workspaces

--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@ rm -rf ~/Library/Application\ Support/Slack/Cache
 rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
 rm -rf ~/Library/Application\ Support/Code/Crashpad
 rm -rf ~/Library/Application\ Support/Code/Cache
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage
 rm -rf ~/Library/Application\ Support/Code/CachedData
 rm -rf ~/Library/Application\ Support/Chrome/Default
