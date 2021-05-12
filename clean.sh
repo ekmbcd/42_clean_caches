b44bc79be44955112e087e5ae595623698f63199
Andrea De felice <ade-feli@e4r2p4.42roma.it>
2021-05-12 16:59:15 +0200

    docker and coresimulator

--- clean.sh
+++ clean.sh
@@ -6,3 +6,5 @@ rm -rf ~/Library/Application\ Support/Code/Cache
 rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage
 rm -rf ~/Library/Application\ Support/Code/CachedData
 rm -rf ~/Library/Application\ Support/Chrome/Default
+rm -rf ~/Library/Developer/CoreSimulator
+rm -rf ~/Library/Containers/com.docker.docker
